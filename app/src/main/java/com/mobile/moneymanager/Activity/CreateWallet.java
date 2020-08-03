a018cadacde5be6dec8d18e899d5c30655f7e846
Quang0202 <quang.dovan0202@gmail.com>
2020-08-04 00:13:07 +0700

    add splash screen

---
+++ app/src/main/java/com/mobile/moneymanager/Activity/CreateWallet.java
@@ -0,0 +1,17 @@
+package com.mobile.moneymanager.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobile.moneymanager.R;
+
+
+public class CreateWallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_wallet);
+    }
+}
---
+++ app/src/main/java/com/mobile/moneymanager/Activity/SplashScreen.java
@@ -0,0 +1,49 @@
+package com.mobile.moneymanager.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.mobile.moneymanager.R;
+
+public class SplashScreen extends AppCompatActivity implements Animation.AnimationListener {
+    private static int SPLASH_TIME_OUT = 3000;
+    ImageView imageView;
+    Animation animation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        imageView= findViewById(R.id.logo);
+        animation= AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_splash);
+        animation.setAnimationListener(this);
+        imageView.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
